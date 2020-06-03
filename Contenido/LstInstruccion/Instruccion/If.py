0ea0fc21bdbc3a26d4a29b914a8ca2d4d65412cb
AndhySolis <49577095+AndhySolis@users.noreply.github.com>
2020-06-03 08:56:37 -0600

    Gui

--- Contenido/LstInstruccion/Instruccion/If.py
+++ Contenido/LstInstruccion/Instruccion/If.py
@@ -2,32 +2,33 @@ from ..ABCInstruccion import Instruccion, ListaInstruccion
 from Contenido.LstInstruccion.Registro.Valor import Valor
 from Contenido.LstInstruccion.ABCInstruccion import Ts
 
+
 class If(Instruccion):
     cuerpo: Instruccion = None
     booleano: Instruccion = None
-    deten : int =0
+    deten: int = 0
+
     def detener_ejecucion(self):
         return self.deten
 
-    def __init__(self,booleano:Instruccion,cuerpo:Instruccion):
-        self.cuerpo=cuerpo
-        self.booleano=booleano
+    def __init__(self, booleano: Instruccion, cuerpo: Instruccion):
+        self.cuerpo = cuerpo
+        self.booleano = booleano
 
     def ejecutar(self):
-        condicion : Valor=self.booleano.ejecutar()
+        condicion: Valor = self.booleano.ejecutar()
         if condicion.tipo == 0:
-            if condicion.dar_valor()==1:
+            if condicion.dar_valor() == 1:
                 self.cuerpo.ejecutar()
-                self.deten=self.cuerpo.detener_ejecucion()
+                self.deten = self.cuerpo.detener_ejecucion()
         else:
-            global  Ts
-            Ts.cargar_error("En La Condicional Del IF Se Necesita El Tipo ENTERO ",3)
-
+            global Ts
+            Ts.cargar_error("En La Condicional Del IF Se Necesita El Tipo ENTERO ", 3)
 
     def str_arbol(self):
         rst = str(id(self)) + "[shape=rect,sides=4,skew=.4,label=\"" + "If  )\"]\n"
         rst += self.booleano.str_arbol()
         rst += str(id(self)) + " -> " + str(id(self.booleano)) + "\n"
-        rst +=self.cuerpo.str_arbol()
+        rst += self.cuerpo.str_arbol()
         rst += str(id(self)) + " -> " + str(id(self.cuerpo)) + "\n"
         return rst
--- Main.py
+++ Main.py
@@ -1,8 +1,23 @@
 import ply.lex as lex
 import ply.yacc as yacc
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from Gui import Ui_MainWindow
 from Contenido.LstInstruccion.ABCInstruccion import Ts
 from Contenido.Analizadores.Lexico import *
 from Contenido.Analizadores.Sintactico import *
+
+def cargar_ventana():
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+    return app
+
+
 
 # Construyendo el analizador léxico
 lexer = lex.lex()
@@ -14,9 +29,13 @@ input: str = f.read()
 # print(input)
 global Ts
 raiz_produccion: ABCInstruccion.ListaInstruccion = parser.parse(input)
+ventana:Ui_MainWindow =cargar_ventana()
 
 Ts.cargar_etiquetas(raiz_produccion)
 Ts.ejecutar_main()
+
+#
+
 #print(raiz_produccion.str_arbol_encabezado())
 #raiz_produccion.ejecutar()
 # for Prod in raiz_produccion:
